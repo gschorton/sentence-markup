15966d0b73fcaf440fd3eb238bca794dcf159fe3
gschorton <gschorton@gmail.com>
2026-05-04 15:58:58 -0400

    removed console.log and added comments

--- sentence-maker/sort-word-classes.mjs
+++ sentence-maker/sort-word-classes.mjs
@@ -13,22 +13,22 @@ export let word_classes_sorted =[];
 export function sortWordClasses(data) {
 
 	word_classes_sorted.length = 0;
-	
+
 	for (let i = 0; i < word_classes_tosort.length; i++) {
 		word_classes_sorted.push([word_classes_tosort[i][0], word_classes_tosort[i][1]]);
 	}
 
+	// Sort by start index, then alpha, then length
 	if(word_classes_sorted.length > 0){
 		word_classes_sorted.sort((a, b) => (a[1][0] - b[1][0]) || 
 								a[0].localeCompare(b[0]) ||
 								b[1].length - a[1].length);
 	}
-	
+
 	// Rename classes according to new sorted list
 	for (let i = 0; i < word_classes_sorted.length; i++) {
 		word_classes_sorted[i][0] = word_classes_sorted[i][0].slice(0, word_classes_sorted[i][0].lastIndexOf("-")) + "-" + i;
 	}
 
 	console.log("Number of classes sorted: "+word_classes_sorted.length);
-	console.log(...word_classes_sorted);
 }
